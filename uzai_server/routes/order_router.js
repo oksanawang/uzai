100726e500e4413902d084fe17418c993fb0d21e
Badly <badly.@Badly.local>
2019-09-21 17:45:49 +0800

    更新呢

--- uzai_server/routes/order_router.js
+++ uzai_server/routes/order_router.js
@@ -2,23 +2,14 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../pool");
 
-router.post("/reg",(req,res)=>{
-  var obj = req.body;
-  console.log(obj);
-  var sql = `INSERT INTO xz_user SET ? `;
-  pool.query(sql,[obj],function(err,result){
-    if(err) throw err;
-    if(result.affectedRows>0){
-      res.send('注册成功')
-    }
-  })
-})
-router.get("/login",(req,res)=>{
+
+router.get("/order",(req,res)=>{
   var obj = req.query;
   console.log(obj)
   // res.send(10086)
-  var uname = obj.uname;
-  var upwd = obj.upwd ;
+  var man_child = obj.man_count;
+  var child_count = obj.cli_count;
+  
   var sql = `SELECT uname=? and upwd=? FROM uzi_user `
   pool.query(sql,[uname,upwd],(err,result)=>{
     if(err)throw err;
@@ -28,6 +19,6 @@ router.get("/login",(req,res)=>{
       res.send("登陆成功")
     }
   })
-
 })
+
 module.exports=router;
